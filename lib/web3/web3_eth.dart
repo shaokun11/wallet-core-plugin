21f1e9bab26f05a13c58227c7dface24f4d0d668
shaokun <shaokuning@gmail.com>
2022-08-10 08:23:52 +0800

    update web3_eth to support all contract call

--- lib/web3/web3_eth.dart
+++ lib/web3/web3_eth.dart
@@ -2,14 +2,20 @@ import 'dart:typed_data';
 import 'package:http/http.dart';
 import 'package:convert/convert.dart';
 import 'package:flutter_trust_wallet_core/protobuf/Ethereum.pb.dart'
-as Ethereum;
+    as Ethereum;
 import 'package:flutter_trust_wallet_core/web3/hex_utils.dart';
 import 'package:web3dart/web3dart.dart';
 import '../flutter_trust_wallet_core.dart';
 import '../trust_wallet_core_ffi.dart';
-import 'contract_abi.dart';
+export './contract_abi.dart';
 
-enum SupportToken { ERC20, ERC721, ERC1155 }
+class Web3EthContract {
+  const Web3EthContract(this.abi, this.name, this.address);
+
+  final String abi;
+  final String name;
+  final String address;
+}
 
 class Web3Eth {
   late final String url;
@@ -27,47 +33,33 @@ class Web3Eth {
         Uint8List.fromList(hex.decode(privateKey)));
   }
 
-  DeployedContract _getContract(String token, SupportToken st) {
-    var abiStr = ERC20Token;
-    switch (st) {
-      case SupportToken.ERC20:
-        abiStr = ERC20Token;
-        break;
-      case SupportToken.ERC721:
-        abiStr = ERC721Token;
-        break;
-      case SupportToken.ERC1155:
-        abiStr = ERC1155Token;
-        break;
-    }
-    final contract = DeployedContract(
-        ContractAbi.fromJson(abiStr, st.name), EthereumAddress.fromHex(token));
-    return contract;
-  }
-
   getNonce(String address) {
     return this
         ._ethClient
         .getTransactionCount(EthereumAddress.fromHex(address));
   }
 
-  EthereumAddress encodingAddress(String address) {
-    return EthereumAddress.fromHex(address);
-  }
-
   getBalance(String address) {
     return this._ethClient.getBalance(EthereumAddress.fromHex(address));
   }
 
-  buildContractHexData(SupportToken st, String _contract, String method,
-      List<dynamic> params) {
-    final contract = this._getContract(_contract, st);
+  EthereumAddress encodingAddress(String address) {
+    return EthereumAddress.fromHex(address);
+  }
+
+  DeployedContract newContract(Web3EthContract contract) {
+    return DeployedContract(ContractAbi.fromJson(contract.abi, contract.name),
+        EthereumAddress.fromHex(contract.address));
+  }
+
+  buildContractHexData(
+      DeployedContract contract, String method, List<dynamic> params) {
     final function = contract.function(method);
     return HexUtils.bytesToHex(function.encodeCall(params));
   }
 
-  estimateGas(String from, String to, BigInt gasPrice,
-      String hexData, {BigInt? value}) {
+  estimateGas(String from, String to, BigInt gasPrice, String hexData,
+      {BigInt? value}) {
     value = value != null ? value : BigInt.from(0);
     return this._ethClient.estimateGas(
         to: EthereumAddress.fromHex(to),
@@ -81,7 +73,7 @@ class Web3Eth {
       BigInt gasLimit, String privateKey) async {
     int coin = TWCoinType.TWCoinTypeEthereum;
     var pk =
-    PrivateKey.createWithData(Uint8List.fromList(hex.decode(privateKey)));
+        PrivateKey.createWithData(Uint8List.fromList(hex.decode(privateKey)));
     var signerInput = Ethereum.SigningInput(
         chainId: HexUtils.int2Bytes(BigInt.from(this.chainId)),
         nonce: HexUtils.int2Bytes(nonce),
@@ -101,24 +93,18 @@ class Web3Eth {
     return this._ethClient.sendRawTransaction(HexUtils.hexToBytes(signTx));
   }
 
-  call(String from, String token, SupportToken st, String method,
-      List<dynamic> params) {
-    final contract = this._getContract(token, st);
+  call(DeployedContract contract, String method, List<dynamic> params,
+      String from) {
     final fun = contract.function(method);
     return this._ethClient.call(
-        contract: this._getContract(token, st),
+        contract: contract,
         function: fun,
         params: params,
         sender: EthereumAddress.fromHex(from));
   }
 
-  send(String from,
-      String contract,
-      String hexData,
-      BigInt gasPrice,
-      BigInt gasLimit,
-      BigInt nonce,
-      String privateKey,
+  send(String from, String contract, String hexData, BigInt gasPrice,
+      BigInt gasLimit, BigInt nonce, String privateKey,
       {BigInt? value}) async {
     int coin = TWCoinType.TWCoinTypeEthereum;
     var ethValue;
