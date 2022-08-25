5c21e308d228824bb0bf201eb49ab3d076866b91
shaokun <shaokuning@gmail.com>
2022-08-25 18:00:56 +0800

    更新solana metadata

--- lib/web3/web3_solana.dart
+++ lib/web3/web3_solana.dart
@@ -3,6 +3,8 @@ import 'dart:typed_data';
 import 'package:convert/convert.dart';
 import 'package:flutter_trust_wallet_core/protobuf/Solana.pb.dart' as Solana;
 import 'package:flutter_trust_wallet_core/web3/http_helper.dart';
+import 'package:solana/metaplex.dart';
+import 'package:solana/solana.dart';
 import '../flutter_trust_wallet_core.dart';
 import '../trust_wallet_core_ffi.dart';
 import 'package:fixnum/fixnum.dart' as $fixed_num;
@@ -10,9 +12,11 @@ import 'package:fixnum/fixnum.dart' as $fixed_num;
 class Web3Solana {
   late final String url;
   int coin = TWCoinType.TWCoinTypeSolana;
+  late RpcClient client;
 
   Web3Solana(String url) {
     this.url = url;
+    this.client = RpcClient(url);
   }
 
   PrivateKey _hexToPrivateKey(String privateKey) {
@@ -30,8 +34,13 @@ class Web3Solana {
 
   getSPLTokenBalance(String account, String token) {
     var tokenAddress =
-    SolanaAddress.createWithString(account).defaultTokenAddress(token);
+        SolanaAddress.createWithString(account).defaultTokenAddress(token);
     return HttpHelper.sendRpc(url, "getTokenAccountBalance", tokenAddress!);
+  }
+
+  getTokenMetadata(String token) {
+    GetMetaplexMetadata(client)
+        .getMetadata(mint: Ed25519HDPublicKey.fromBase58(token));
   }
 
   sendSOl(String nonce, String to, BigInt amount, String privateKey) {
@@ -51,9 +60,9 @@ class Web3Solana {
   sendToken(String from, String to, String token, BigInt amount, int decimals,
       String nonce, String privateKey) {
     var fromTokenAddress =
-    SolanaAddress.createWithString(from).defaultTokenAddress(token);
+        SolanaAddress.createWithString(from).defaultTokenAddress(token);
     var toTokenAddress =
-    SolanaAddress.createWithString(to).defaultTokenAddress(token);
+        SolanaAddress.createWithString(to).defaultTokenAddress(token);
     final pk = _hexToPrivateKey(privateKey);
     final signInput2 = Solana.SigningInput(
         recentBlockhash: nonce,
@@ -65,7 +74,7 @@ class Web3Solana {
             amount: $fixed_num.Int64.parseInt(amount.toString()),
             decimals: decimals));
     final sign =
-    AnySigner.sign(signInput2.writeToBuffer(), TWCoinType.TWCoinTypeSolana);
+        AnySigner.sign(signInput2.writeToBuffer(), TWCoinType.TWCoinTypeSolana);
     final signOutput = Solana.SigningOutput.fromBuffer(sign.toList());
     final signTx = signOutput.encoded;
     return HttpHelper.sendRpc(url, "sendTransaction", signTx);
